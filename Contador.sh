0d5d2c4f4ea8ecfeae188b7588053fa947456717
Alan.MS <42683597+JumpAMS@users.noreply.github.com>
2018-12-04 15:52:30 -0200

    Update Contador.sh

--- Contador.sh
+++ Contador.sh
@@ -6,8 +6,8 @@ for (( loop=0;loop=1000;loop++ )) do
 			TEMPOS=$(($TEMPOS+1))				
 			clear
 			echo '[ATENÇÃO] O ataque está sendo realizado!'
-			echo '[ATENÇÃO] Quando o ataque for interrompido, utilize o comando: ~$ diedown 
-			para acessar novamente a internet'
+			echo '[ATENÇÃO] Quando o ataque for interrompido, utilize o comando: ~$ diedown'
+			echo 'para acessar novamente a internet'
 			echo ' '
 			echo '[+] Para interromper pressione ( Ctrl + C )'
 			echo ' '
