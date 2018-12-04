eb85a3f218e7b9696ec232818b5a03d5c4ad851f
Alan.MS <42683597+JumpAMS@users.noreply.github.com>
2018-12-04 15:59:36 -0200

    Update DieDown.sh

--- DieDown.sh
+++ DieDown.sh
@@ -2,7 +2,10 @@
 
 PLACA3=`ifconfig | grep w | cut -d ":" -f1`
 
-airmon-ng stop $PLACA3 > /del/null 2>&1
+sleep 1
+
+airmon-ng stop $PLACA3 > /dev/null
+
 echo ' '
 echo '[+] Você já pode acessar a internet normalmente !'
 echo ' '
